2ab639e4ef7f32719fe5d2e920734e6400fa561b
Emirhan Gök <75798978+Emiran404@users.noreply.github.com>
2026-03-19 04:18:12 +0300

    Remove encryption message from startup script

--- baslat.bat
+++ baslat.bat
@@ -3,6 +3,6 @@ echo ======================================================
 echo   PolyOS Sinav Platformu - Baslatiliyor (PROD)
 echo ======================================================
 
-echo Uygulama üretim modunda (şifreli kodlarla) başlatılıyor...
+echo Uygulama üretim modunda başlatılıyor...
 echo.
 call npm run preview:full
